3a21882c59d907b3e501752e7c66883c757e1920
Jacob Niemeir <nniemeir@protonmail.com>
2025-08-15 09:46:43 -0500

    Now checks for history file existence before trying to read it

--- src/history.c
+++ src/history.c
@@ -1,5 +1,6 @@
 #include "history.h"
 #include "error.h"
+#include "file.h"
 
 char *init_history(char *home_dir) {
   char *hist_file = malloc(PATH_MAX);
@@ -11,6 +12,10 @@ char *init_history(char *home_dir) {
   snprintf(hist_file, PATH_MAX, "%s/.clown_scribbles", home_dir);
 
   using_history();
+
+  if (!file_exists(hist_file)) {
+    return hist_file;
+  }
 
   if (read_history(hist_file) != 0) {
     error_msg("Failed to read history", false);
